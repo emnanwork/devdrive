f104b6aa994ba89af5213c3c3fd6d6c072ff354c
Emnan Work <emnanwork@gmail.com>
2024-08-02 15:11:29 +0200

    redo requirement file

--- drf/views.py
+++ drf/views.py
@@ -6,11 +6,35 @@ from blogapp.models import Blog
 from drf.serializers import BlogSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
 
-#using function views
 
+#class based views
+class BlogDetailAPIView(APIView):
+    def get(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data) 
+    
+    def put(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        serializer = BlogSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def delete(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk) 
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+#using function views
 @api_view(['GET', 'POST'])
 def blog_list(request):
     if request.method == 'GET':
